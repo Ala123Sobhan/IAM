c43144e9f1a35c76bb32af77ef636b11dfc08166
Ala123Sobhan <ala123sobhan@gmail.com>
2023-06-10 22:00:13 -0500

    commands

--- commands.sh
+++ commands.sh
@@ -8,6 +8,10 @@ aws cloudformation describe-stacks --stack-name myrole101
 
 aws iam simulate-principal-policy --policy-source-arn arn:aws:iam::YOUR_ACCOUNT_ID:role/Role1 --action-names "iam:CreateRole" "iam:ListRoles" "iam:SimulatePrincipalPolicy" "ec2:DescribeImages" "ec2:RunInstances" "ec2:DescribeSecurityGroups"
 aws sts assume-role --role-arn arn:aws:iam::199055471403:role/MyRole301 --role-session-name tp
+aws --profile assumed_role_profile <AWS_CLI_COMMAND>
 
 aws s3api list-buckets
 aws s3 cp local_file.txt s3://bucket_name/path/to/destination_file.txt
+aws s3api create-bucket --bucket hobena --region us-west-2
+aws configure list-profiles
+
