406f223658a2f0c7bedb78181f696bcd33ac6d7a
Ala123Sobhan <ala123sobhan@gmail.com>
2023-06-10 20:25:25 -0500

    trust policy assumed by role

--- commands.sh
+++ commands.sh
@@ -8,3 +8,6 @@ aws cloudformation describe-stacks --stack-name myrole101
 
 aws iam simulate-principal-policy --policy-source-arn arn:aws:iam::YOUR_ACCOUNT_ID:role/Role1 --action-names "iam:CreateRole" "iam:ListRoles" "iam:SimulatePrincipalPolicy" "ec2:DescribeImages" "ec2:RunInstances" "ec2:DescribeSecurityGroups"
 aws sts assume-role --role-arn arn:aws:iam::199055471403:role/MyRole301 --role-session-name tp
+
+aws s3api list-buckets
+aws s3 cp local_file.txt s3://bucket_name/path/to/destination_file.txt
